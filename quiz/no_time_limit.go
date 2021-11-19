ed8bb49139b06607999bd9571ad24abbcbf7204f
raptor72 <mctaras51@yandex.ru>
2021-11-19 18:43:47 +0400

    Initial commit

---
+++ quiz/no_time_limit.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+    "io"
+    "os"
+    "fmt"
+    "log"
+    "encoding/csv"
+//    "strings"
+    "strconv"
+)
+
+/*
+func countResult(row []string) int {
+    result, err :=  strconv.Atoi(row[1])
+    if err != nil {
+        log.Fatal("error of type result")
+    }
+    sumString := strings.Split(row[0], "+")
+    first, err := strconv.Atoi(sumString[0])
+    if err != nil {
+        log.Fatal("error of type firstString")
+    }
+    second, err := strconv.Atoi(sumString[1])
+    if err != nil {
+        log.Fatal("error of type secondString")
+    }
+    if first + second == result {
+        return 1
+    } else {
+        return 0
+    }
+}
+*/
+
+func readQuestions(row []string) string {
+    if len(row) != 2 {
+        log.Fatal("Wrong row length")
+    }
+    return row[0]
+}
+
+func parseAnswer(row []string) int {
+    if len(row) != 2 {
+        log.Fatal("Wrong row length")
+    }
+    result, err :=  strconv.Atoi(row[1])
+    if err != nil {
+        log.Fatal("error of type result")
+    }
+    return result
+}
+
+func main() {
+    var count, rights, userAnswer int
+    file, err := os.Open("problems.csv")
+//    file, err := os.Open("test.csv")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    r := csv.NewReader(file)
+
+    for {
+        row, err := r.Read()
+        if err != nil && err != io.EOF {
+            log.Fatal(err)
+        }
+        if err == io.EOF {
+//            fmt.Println("read all file")
+            break
+        }
+//        fmt.Println(row[0])
+//        fmt.Printf("%T \n", row)
+//        fmt.Println(len(row))
+        if len(row) != 2 {
+            log.Fatalf("Uncoreect row length. Should be 2 but found: %v", len(row))
+        }
+        fmt.Println(readQuestions(row))
+        fmt.Scan(&userAnswer)
+        if parseAnswer(row) == userAnswer {
+            rights++
+        }
+        count++
+    }
+    fmt.Printf("Total questions is: %v, right questions is: %v\n", count, rights)
+}
