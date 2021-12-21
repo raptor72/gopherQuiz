30339e4caf154c3f7613f645ad3ac02d2e911ebb
tarasov <tarasov@o-code.ru>
2021-12-21 18:55:11 +0400

    add test

--- urlshort/handler_test.go
+++ urlshort/handler_test.go
@@ -1,12 +1,8 @@
 package urlshort
 
 import (
-//        "bytes"
-//        "io"
         "fmt"
-//        "os"
         "testing"
-//        "io/ioutil"
 )
 
 
@@ -18,7 +14,7 @@ func TestBytesToString(t *testing.T) {
             t.Fatalf("Want %v\n, but got %v", expected_string, st)
         } else {
             fmt.Println("TestBytesToString: ")
-            fmt.Printf("Want string `%v`, and got `%v`. Ok. \n\n", expected_string, st)
+            fmt.Printf("Want string '%v', and got the same string '%v'.\n\n", expected_string, st)
         }
 }
 
@@ -38,13 +34,13 @@ func TestParseYAMLsingleBlock(t *testing.T) {
             }
         }
         fmt.Println("TestParseYAMLsingleBlock: ")
-        fmt.Printf("Got correct path and url from yaml in []bytes.\n\n")
+        fmt.Printf("Got correct path and url from single yaml block in bytes.\n\n")
 }
 
 
 func TestParseYAMLmultipleBlock(t *testing.T) {
         multiple_yaml := []byte("- path: /short_path1\n  url: https://url1\n- path: /short_path2\n  url: https://url2")
-        expected_yaml_struct := []yamlStruct{yamlStruct{"/short_path1", "https://url1"}, yamlStruct{"/short_path2", "https://url2"} }
+        expected_yaml_struct := []yamlStruct{yamlStruct{"/short_path1", "https://url1"}, yamlStruct{"/short_path2", "https://url2"}}
         ya, err := parseYAML(multiple_yaml)
         if err != nil {
             t.Fatalf("Get a parsing yaml error: %v", err)
@@ -58,5 +54,24 @@ func TestParseYAMLmultipleBlock(t *testing.T) {
             }
         }
         fmt.Println("TestParseYAMLmultipleBlock: ")
-        fmt.Printf("Got correct path and url from yaml in []bytes.\n\n")
+        fmt.Printf("Got correct path and url from multiple yaml blocks in bytes.\n\n")
 }
+
+
+func TestBuildMap(t *testing.T) {
+    yaml_struct := []yamlStruct{yamlStruct{"/short_path1", "https://url1"}, yamlStruct{"/short_path2", "https://url2"}}
+    expected_map := map[string]string{
+    "/short_path1": "https://url1",
+    "/short_path2": "https://url2",
+    }
+    recieved_map := buildMap(yaml_struct)
+    for key, value := range recieved_map {
+        if expected_map[key] != value {
+            t.Fatalf("Get wrong map. Want %v, but got %v", expected_map[key], value)
+        }
+    }
+    fmt.Println("TestBuildMap: ")
+    fmt.Printf("Got correct map.\n\n")
+}
+
+
